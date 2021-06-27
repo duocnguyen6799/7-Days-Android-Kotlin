406f249f95e5c2df85a0bf3bbd8ce16a640c4b5a
Nguyễn Phục Dược <41044684+duocnguyen6799@users.noreply.github.com>
2021-06-27 12:29:38 +0700

    Day 03 - Material Design, Custom Theme & ConstraintLayout

---
+++ Day03/app/src/main/java/com/npdstudio/day03/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.npdstudio.day03
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.npdstudio.day03.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnCalculate.setOnKeyListener { v, keyCode, _ ->  hideKeyboard(v, keyCode)}
+    }
+    private fun hideKeyboard(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+}
