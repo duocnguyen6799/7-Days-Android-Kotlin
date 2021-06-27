6580534e0e6d429e21bd3c471a5590d7c11318fa
Nguyen Phuc Duoc <duocnguyen6799@gmail.com>
2021-06-28 04:51:55 +0700

    Day 03 - Optimize code

--- Day03/app/src/main/java/com/npdstudio/day03/MainActivity.kt
+++ Day03/app/src/main/java/com/npdstudio/day03/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.npdstudio.day03
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.npdstudio.day03.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnCalculate.setOnKeyListener { v, keyCode, _ ->  hideKeyboard(v, keyCode)}
+        binding.btnCalculate.setOnKeyListener { v, keyCode, _ -> hideKeyboard(v, keyCode) }
     }
+
     private fun hideKeyboard(view: View, keyCode: Int): Boolean {
         if (keyCode == KeyEvent.KEYCODE_ENTER) {
             // Hide the keyboard
             val inputMethodManager =
-                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
             inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
             return true
         }
